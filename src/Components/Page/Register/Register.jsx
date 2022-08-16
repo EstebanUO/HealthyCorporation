f5ec357f52eff9594b0fb73554658e0b84524034
EstebanUO <esteban.urrego@misena.edu.co>
2022-08-16 16:44:56 -0500

    Se añadio la vista de registro

---
+++ src/Components/Page/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Footer } from '../Layout/Footer/Footer'
+import { Header } from '../Layout/Header/Header'
+
+export const register = () => {
+  return (
+    <div>
+        <Header/>
+        <h1>Register</h1>
+        <Footer/>
+    </div>
+  )
+}
