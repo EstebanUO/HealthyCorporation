815ae28ff8d17050f62a4f0193c1a9b16efe903d
Esteban <esteban.urrego@misena.edu.co>
2022-08-17 11:40:27 -0500

    Se agrego al vista de login y cambios en register.EU

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import {Products} from '../src/Components/Page/Products/Products';
 import {Register} from '../src/Components/Page/Register/Register';
 import {Terminos} from '../src/Components/Page/Terminos_Condiciones/Terminos';
+import { Login } from './Components/Page/Login/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/products" element={<Products />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/terminos" element={<Terminos />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </div>
     
