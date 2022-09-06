5a4c8ce832e2483c9a9ffb69d0fa6623feca9485
jc4milo <jc4milo311@gmail.com>
2022-09-06 06:26:46 -0500

    home jc4

--- src/Components/Page/Home/Home.jsx
+++ src/Components/Page/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Header } from '../Layout/Header/Header';
 import { Footer } from '../Layout/Footer/Footer';
+import "../Home/Home.css";
+import Slider from '../../UI/SliderHome/SliderHome';
+
+
 
 export const Home = () => {
   return (
     <div>
         <Header />
+          <div>
+              <h1>Productos más vendidos</h1>
+              <Slider />
+          </div>
         <Footer />
     </div>
   )
