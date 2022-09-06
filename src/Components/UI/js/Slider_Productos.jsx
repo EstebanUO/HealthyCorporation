bf33de871e823c4255a9e016685693dd51ae2d78
Sebas <xsebaxz200303@gmail.com>
2022-09-06 07:48:54 -0500

    Nuevo cambio SG

--- src/Components/UI/js/Slider_Productos.jsx
+++ src/Components/UI/js/Slider_Productos.jsx
@@ -127,17 +127,5 @@ export const Slider_Productos = () => {
     position: absolute;
     transition: .3s ease all;
 
-<<<<<<< HEAD
     ${props => props.derecho ? 'right: 0' : 'left: 0'}
     `;
-
-
-    
-=======
-    path{
-      filter: ${props.derecho ? 'drop-shadow(-2px 0px 0px #fff)': 'drop-shadow(2px 0px 0px #fff'};
-    }
-
-    ${props => props.derecho ? 'right: 0' : 'left: 0'}
-    `;
->>>>>>> Sebas
