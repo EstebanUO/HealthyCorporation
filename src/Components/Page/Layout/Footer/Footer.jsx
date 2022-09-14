69f6942e2ac00dbb9da3a45a245d02188193873f
jc4milo <jc4milo311@gmail.com>
2022-09-14 09:12:15 -0500

    actializacion

--- src/Components/Page/Layout/Footer/Footer.jsx
+++ src/Components/Page/Layout/Footer/Footer.jsx
@@ -5,12 +5,12 @@ import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-k
 
 export const Footer = () => {
     return (
-<<<<<<< HEAD
-      <MDBFooter bgColor='light' className='text-center text-lg-start text-muted' >
+
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted' >
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom' style={{backgroundColor: "#83c5eb", borderTop: "solid black"}}>
         <div className='me-5 d-none d-lg-block'>
           <span>Mira nuestras redes sociales:</span>
-=======
+
         <MDBFooter className='text-center' color='balk' style={{ backgroundColor: '#83c5eb',borderTop: 'solid black' }}>
         <MDBContainer className='p-4'>
           <section className='mb-4'>
@@ -184,7 +184,7 @@ export const Footer = () => {
   
         <div className='text-center p-3' style={{ backgroundColor: 'rgb(83 194 255 / 84%)', borderTop: 'solid black' }}>
           <a className='text-white' href='/'>Healthy Corporation</a> © 2020 Copyright 
->>>>>>> 0cc6c6829c1264aeb9d14d405b807dfe196e5382
+
         </div>
 
         <div >
@@ -298,3 +298,4 @@ export const Footer = () => {
     </MDBFooter>
     )
 }
+
