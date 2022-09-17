bca9c745dfcb0a4a5bdc2367d4474b898f95810a
EstebanUO <esteban.urrego@misena.edu.co>
2022-09-17 15:09:55 -0500

    Se añadio el archivo navbar.EU

---
+++ src/Components/UI/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaUserAlt,FaShoppingCart,FaMapMarkerAlt,FaHeartbeat,FaSistrix } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+export const NavBar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            <div className='navbar2'>
+                <FaHeartbeat/><p>Lista de Deseos</p>
+            </div>
+            <div className='navbar2'>
+                <FaMapMarkerAlt/><p>Ubicación</p>
+            </div>
+            <div className='navbar2'>
+                <input type="text" placeholder='¿Qué deseas buscar?' className='buscar'/><button className='buscar2'><FaSistrix/></button> 
+            </div>
+            <div className='navbar2'>
+                <FaUserAlt/><Link to='/register' className='cuenta'>Mi cuenta</Link> 
+            </div>
+            <div className='navbar2'>
+                <FaShoppingCart/><p>[cantidad]</p>
+            </div>
+        </div>
+    </div>
+  )
+}
