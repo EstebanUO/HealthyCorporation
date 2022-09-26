40b7fe3a068c72e0c353ab8a52b97492b229ba63
EstebanUO <esteban.urrego@misena.edu.co>
2022-09-26 18:21:54 -0500

    Terminar lista deseos.EU

---
+++ src/Components/Page/ListaDeseos/ListaDeseos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from '../Layout/Header/Header';
+import { Footer } from '../Layout/Footer/Footer'
+
+export const ListaDeseos = () => {
+  return (
+    <div>
+        <Header/>
+        <h1>Tus productos favoritos</h1>
+        <Footer/>
+    </div>
+  )
+}
