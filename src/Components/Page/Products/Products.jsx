9c885df3c8d7b48506e3bcbbdadc71d9019703ea
Esteban <esteban.urrego@misena.edu.co>
2022-10-12 11:22:04 -0500

    Cambios en la vista productos.EU

--- src/Components/Page/Products/Products.jsx
+++ src/Components/Page/Products/Products.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Header } from '../Layout/Header/Header';
 import { Footer } from '../Layout/Footer/Footer';
 // import Slider from '../../UI/js/Slider';
-import { Card } from '../../UI/Card/Card';
+import { CardAllProducts } from '../../UI/CardAllProducts/CardAllProducts';
 import { FaArrowDown, FaSlidersH,FaTh,FaThList } from "react-icons/fa";
 import { Categories } from '../../UI/Categories/Categories';
 
@@ -10,8 +10,8 @@ export const Products = (props) => {
 
   return (
     <div>
-      <Header valiLoginAdmin={props.valiLoginAdmin} />
-      <h2>Compra por categoría</h2>
+      <Header valiLoginAdmin={props.valiLoginAdmin} /><br /><br />
+      <h2 className='titleCategoria'>Compra por categoría</h2>
       <div className='conteinerProducts'>
         <Categories/>
         <div className='containerFilter'>
@@ -29,9 +29,21 @@ export const Products = (props) => {
             <input type="text" id='priceRange' readOnly/>
             <div id='price-range' className='sliderFilter'></div>
         </div>
-        <Card />
+        <div className='container-Orden-Productos'>
+          <p>6363 | Productos Encontrados</p> 
+          | <FaTh/> | <FaThList/> | 
+          <p>Ordenar por:</p>
+          <select name="" id="">
+            <option value="">Relevancia</option>
+            <option value="">Más vendidos</option>
+            <option value="">Más recientes</option>
+            <option value="">Descuento</option>
+            <option value="">Precio más alto</option>
+            <option value="">Precio más bajo</option>
+          </select>
+        </div>
+        <CardAllProducts />
       </div>
-      
       <Footer />
     </div>
   )
---
+++ src/Components/UI/CardAllProducts/CardAllProducts.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import medicasp from '../../Image/assects/medicasp.jpg';
+import pax from '../../Image/assects/pax.jpg';
+import nutribela from '../../Image/assects/nutribela.jpg';
+import acetaminofen from '../../Image/assects/acetaminofen.jpg';
+import { Link } from "react-router-dom";
+import { FaShoppingCart, FaArrowDown, FaSlidersH,FaTh,FaThList } from "react-icons/fa";
+import swal from 'sweetalert';
+
+export const CardAllProducts = () => {
+  const add = () => {
+    swal({
+      title: "Producto agregado al carrito",
+      text: "Se ha añadido correctamente",
+      icon: "success",
+      button: "Seguir comprando",
+    });
+  }
+
+  return (
+    <div className='content_Product'>
+      {/* <h1 className='title-product'>Productos más comprados</h1> */}
+      <div className='card_all_product'>
+        <div className='column_all'>
+          <div className='imgProduct_' >
+            <Link to="/compra"><img className='imgProduct' src={medicasp} alt="Producto imagen" /></Link>
+          </div>
+          <div className='padding_product'>
+            <div>
+              <p className='name_Product'>Medicasp </p>
+            </div>
+            <div>
+              <p className='price_product'>$ 40.000</p>
+            </div>
+            <div>
+              <p className='name_Product_'>Cantidad disponible:  4</p>
+            </div>
+            <div className='rowProduct'>
+              <p className='textProduct'>Despacho</p>
+              <p className='textProduct_'>Retiro</p>
+              <p className='textProduct'>Venta directa </p>
+            </div>
+          </div>
+          <div className='addProduct'>
+            <button onClick={add} className='addProduct_'>
+              Agregar <FaShoppingCart />
+            </button>
+          </div>
+        </div>
+        <div className='column_all'>
+          <div className='imgProduct_' >
+          <Link to="/compra"><img className='imgProduct2' src={pax} alt="Producto imagen" /></Link>
+          </div>
+          <div className='padding_product'>
+            <div>
+              <p className='name_Product'>Pax noche </p>
+            </div>
+
+            <div>
+              <p className='price_product'>$ 7.000</p>
+            </div>
+            <div>
+              <p className='name_Product_'>Cantidad disponible:  4</p>
+            </div>
+            <div className='rowProduct'>
+              <p className='textProduct'>Despacho</p>
+              <p className='textProduct_'>Retiro</p>
+              <p className='textProduct'>Venta directa</p>
+            </div>
+          </div>
+          <div className='addProduct'>
+          <button onClick={add} className='addProduct_'>
+              Agregar <FaShoppingCart />
+            </button>
+          </div>
+        </div>
+        <div className='column_all'>
+          <div className='imgProduct_' >
+          <Link to="/compra"><img className='imgProduct' src={nutribela} alt="Producto imagen" /></Link>
+          </div>
+          <div className='padding_product'>
+            <div>
+              <p className='name_Product'>Nutribela 10</p>
+            </div>
+
+            <div>
+              <p className='price_product'>$ 17.000</p>
+            </div>
+            <div>
+              <p className='name_Product_'>Cantidad disponible:  4</p>
+            </div>
+            <div className='rowProduct'>
+              <p className='textProduct'>Despacho</p>
+              <p className='textProduct_'>Retiro</p>
+              <p className='textProduct'>Venta directa</p>
+            </div>
+          </div>
+          <div className='addProduct'>
+          <button onClick={add} className='addProduct_'>
+              Agregar <FaShoppingCart />
+            </button>
+          </div>
+        </div>
+        <div className='column_all'>
+          <div className='imgProduct_' >
+          <Link to="/compra"><img className='imgProduct2' src={acetaminofen} alt="Producto imagen" /></Link>
+          </div>
+          <div className='padding_product'>
+            <div>
+              <p className='name_Product'>Acetaminofén  </p>
+            </div>
+
+            <div>
+              <p className='price_product'>$ 10.000</p>
+            </div>
+            <div>
+              <p className='name_Product_'>Cantidad disponible:  4</p>
+            </div>
+            <div className='rowProduct'>
+              <p className='textProduct'>Despacho</p>
+              <p className='textProduct_'>Retiro</p>
+              <p className='textProduct'>Venta directa</p>
+            </div>
+          </div>
+          <div className='addProduct'>
+          <button onClick={add} className='addProduct_'>
+              Agregar <FaShoppingCart /> 
+            </button>
+          </div>
+        </div>
+      </div><br />
+    </div>
+  )
+}
