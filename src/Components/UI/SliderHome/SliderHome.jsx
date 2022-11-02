162e0105ac2850eace97534aaf1ff882bd34ac77
jc4milo <jc4milo311@gmail.com>
2022-11-02 18:13:14 -0500

    api img jc

--- src/Components/UI/SliderHome/SliderHome.jsx
+++ src/Components/UI/SliderHome/SliderHome.jsx
@@ -1,40 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 import './Slider.css'
 import { motion } from 'framer-motion'
-import images from '../../../exports/images'
+// import images from '../../../exports/images'
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 
 
 const SliderHome = () => {
+  const baseURL = "https://api-products-healthy.herokuapp.com/api/healthyapp";
+  const [img, setimg] = useState([])
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setimg(response.data);
+
+    });
+  }, []);
+
+
 
   return (
     <>
       <motion.div className='slider-container'>
         <motion.div className='slider' drag='x'
           dragConstraints={{ right: 0, left: -420 }} >
-          {images.map(image => (
+
+          {img.map(data => {
             <motion.div className='item'>
               <div class="card">
                 <div class="card-img">
-                <img className='item-image' src={image} alt="Imagen producto" />
+                  <img className='item-image' src={data.imagen} alt="Imagen producto" />
                 </div>
                 <div class="card-info">
-                  <p class="text-title">[Nombre]</p>
-                  <p class="text-body">Lorem Ipsum dolor sit amet</p>
+                  <p class="text-title">{ }</p>
+
+                  <p class="text-body"><b>{ }</b></p>
                   <Link to='/compra'><button class="card-button">Ver más</button></Link>
                 </div>
               </div>
             </motion.div>
-          ))}
+          })
+
+          }
+
+
+
+
         </motion.div>
       </motion.div>
-
-
-
     </>
-
-
   )
 }
 
