313811a8fc9bf3fbd1ddc98523809a4286a38912
Esteban <esteban.urrego@misena.edu.co>
2022-11-11 11:42:33 -0500

    Nuevos cambios.EU

--- src/Components/UI/NavBar/NavBar.jsx
+++ src/Components/UI/NavBar/NavBar.jsx
@@ -38,17 +38,46 @@ export const NavBar = () => {
     }
 
     /*---------------------------------- Menu Hamburguesa -------------------------------------- */
+    const [clickMenu, setClickMenu] = useState(false)
+
+
+    const switchShown2 = () => {
+        setClickMenu(!clickMenu)
+    };
     const line1__bars = document.querySelector(".line1__bars-menu");
     const line2__bars = document.querySelector(".line2__bars-menu");
     const line3__bars = document.querySelector(".line3__bars-menu");
+    const nav2 = document.getElementById("nav2");
+    const nav3 = document.getElementById("nav3");
+    const nav4 = document.getElementById("nav4");
 
-    window.onClick = function () {animateBars()}
+    // window.onClick = function () {animateBars()}
 
     function animateBars(){
+        switchShown2()
         line1__bars.classList.toggle("activeline1__bars-menu");
         line2__bars.classList.toggle("activeline2__bars-menu");
         line3__bars.classList.toggle("activeline3__bars-menu");
+        
     }
+
+    useEffect(() => {
+        if (clickMenu==false) {
+            console.log("menu hamburguesa guardado");
+            console.log(clickMenu);
+            // nav2.classList.toggle("navbar4");
+            // nav3.classList.toggle("navbar4");
+            // nav4.classList.toggle("navbar4");
+        }else if(clickMenu==true){
+            nav2.classList.toggle("navbar2");
+            nav3.classList.toggle("navbar2");
+            nav4.classList.toggle("navbar2");
+            console.log(clickMenu);
+        }
+    }, [clickMenu])
+    
+
+
 
     /*---------------------------------- second nav -------------------------------------- */
 
@@ -77,23 +106,24 @@ export const NavBar = () => {
             <div>
                 <Link to='/'><img src={logo} alt="logo" id='logo' /></Link>
             </div>
-            <div className='navbar2'>
+            <div  className='navbar3'>
                 <input type="text" placeholder='Buscar...' className='buscar' /><button className='buscar2'><FaSistrix /></button>
             </div>
+            <div className='bars__menu'onClick={switchShown2}>
+                <span class="line1__bars-menu"></span>
+                <span class="line2__bars-menu"></span>
+                <span class="line3__bars-menu"></span>
+            </div>
+            {/* <input type="checkbox" id='menu'className='menuInput'/> */}
             <div className="column_nav_content">
-                <div className='navbar2'>
+                <div id='nav2' className='navbar2'>
                     <Link to="/lista-deseos" onClick={handleClick} className={`links ${clicked ? 'active' : ''}`}><FaHeartbeat /> Lista de Deseos </Link>
                 </div>
-                <div className='navbar2'>
+                <div id='nav3' className='navbar2'>
                     <Link to='/register' onClick={handleClick} className={`links ${clicked ? 'active' : ''}`}><FaUserAlt /> Mi cuenta</Link>
                 </div>
-                <div className='navbar2'>
+                <div id='nav4' className='navbar2'>
                     <Link to='/car' onClick={handleClick} className={`links ${clicked ? 'active' : ''}`}><FaShoppingCart />{counter}</Link>
-                </div>
-                <div class="bars__menu" onClick={animateBars}>
-                    <span class="line1__bars-menu"></span>
-                    <span class="line2__bars-menu"></span>
-                    <span class="line3__bars-menu"></span>
                 </div>
             </div>
         </div>
