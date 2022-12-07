822e3b3ac8a452557a08fe39bb32177517d5a6df
Sebastian Gonzalez Moreno <xsebaxz200303@gmail.com>
2022-12-07 09:57:25 -0500

    Responsive casi terminado S.G.M

--- src/Components/Page/Contact/Contact.jsx
+++ src/Components/Page/Contact/Contact.jsx
@@ -29,30 +29,6 @@ export const Contact = (props) => {
                   Tel: 3177896243 | Fax:  01 234 567 88 03 | 
                   e-mail: healthyCorporationContact@gmail.com</h3>
             </div>
-
-            
-
-          {/* <h2 className='text_product_'>Contacta</h2>
-          <h2 className='text_product_2'>con nosotros</h2> */}
-          <div className="about_all">
-            {/* <div className="about_contact">
-              <h2 className="about_contact2">Cuéntanos qué nesecitas</h2>
-              <div className="about_contact3">
-                <h3 className="about_contact4">Rellena el siguiente formulario y nos pondremos en contacto contigo a la mayor brevedad posible.</h3>
-              </div>
-            </div> */}
-            {/* <div className="about_where">
-              <h2 className="about_where1">Dónde estamos</h2>
-              <div className="about_where2">
-                <h3 className="about_where3">Barrio puerto espejo | Caminos de cocora |
-                  Tel: 3177896243 | Fax:  01 234 567 88 03 | 
-                  e-mail: healthyCorporationContact@gmail.com</h3>
-              </div>
-            </div> */}
-          
-          </div>
-
-
         </div>
         <Form />
       </div>
