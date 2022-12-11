e6ce836b281b019ce4106773f3a32cfd9ff53440
jc4milo <jc4milo311@gmail.com>
2022-12-11 15:59:30 -0500

    Actualizacion 3

--- src/Components/Page/Contact/Contact.jsx
+++ src/Components/Page/Contact/Contact.jsx
@@ -10,22 +10,22 @@ export const Contact = (props) => {
     <div>
       <Header valiLoginAdmin={props.valiLoginAdmin} />
       <div className="info_contact">
-        <div style={{ width: "400px"  }}  className="info_all__">
+        <div className="info_all__">
             
             <div>
               <h2 className='text_product_'>Contacta <h2 className='text_product_2'>con nosotros</h2></h2>
               
-            </div>
+            </div><br /><br />
 
-            <div style={{ width:"1", marginTop:"5rem", padding:"30px", }} >
+            <div className='info_2' >
               <h2 className="about_contact2">Cuéntanos qué nesecitas</h2>
-              <h3  className="about_contact4" style={{marginLeft: "1.2rem"}} >Rellena el siguiente formulario y nos pondremos en contacto contigo a la mayor brevedad posible.</h3>
+              <h3  className="about_contact4">Rellena el siguiente formulario y nos pondremos en contacto contigo a la mayor brevedad posible.</h3>
 
-            </div>
+            </div><br /><br />
 
-            <div style={{ width:"1", padding:"30px", }} >
+            <div className='info_2'>
               <h2 className="about_contact2">Dónde estamos</h2>
-              <h3 className="about_where3">Barrio puerto espejo | Caminos de cocora |
+              <h3 className="about_contact4">Barrio puerto espejo | Caminos de cocora |
                   Tel: 3177896243 | Fax:  01 234 567 88 03 | 
                   e-mail: healthyCorporationContact@gmail.com</h3>
             </div>
