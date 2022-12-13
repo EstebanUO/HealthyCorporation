eda3494286972544e83b7ca214052d10a5274df4
feerny <luisfpe19@gmail.com>
2022-12-13 08:32:05 -0500

    elimine linea de politicas.FF

--- src/Components/Page/Politicas_Cond/Politicas.jsx
+++ src/Components/Page/Politicas_Cond/Politicas.jsx
@@ -48,7 +48,7 @@ export const Politicas = (props) => {
                 como el de alta de usuario, puede marcar o desmarcar la opción de recibir información por correo electrónico. En caso de que haya marcado la opción de recibir nuestro boletín o publicidad usted puede cancelarla en cualquier momento.<br/><br/>
                 Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con un orden judicial.<br/><br/>
                 Healthy Corporation Se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.<br/><br/>
-                Esta politica de privacidad se han generado en politicadeprivacidadplantilla.com.</p>
+              </p>
                 
               </div>
             </div>
